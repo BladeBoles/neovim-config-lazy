413984784a6a9a8e1c69cbd965910ebac22b5e34
Blade Boles <bladerboles@gmail.com>
2024-05-04 21:38:12 -0600

    feat: add shortcuts for toggle blame, open and copy file and commit urls

--- lua/plugins/git-blame.lua
+++ lua/plugins/git-blame.lua
@@ -1,3 +1,10 @@
 return {
   "f-person/git-blame.nvim",
+  keys = {
+    { "<leader>gb", "<cmd>GitBlameToggle<cr>", desc = "Toggle [G]it [B]lame" },
+    { "<leader>gyc", "<cmd>GitBlameCopyCommitURL<cr>", desc = "Copy Commit URL" },
+    { "<leader>gyf", "<cmd>GitBlameCopyFileURL<cr>", desc = "Copy File URL" },
+    { "<leader>goc", "<cmd>GitBlameOpenCommitURL<cr>", desc = "Open Commit URL" },
+    { "<leader>gof", "<cmd>GitBlameOpenFileURL<cr>", desc = "Open File URL" },
+  },
 }
