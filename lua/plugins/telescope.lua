8467dc9dcc468928e8f22d6caf263ab15f8f85ce
Blade Boles <bladerboles@gmail.com>
2024-05-09 16:42:29 -0600

    fix: weird results appearance for lsp definitions/references

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,9 +4,18 @@ return {
   -- For major updates, this must be adjusted manually.
   opts = {
     defaults = {
-      -- layout_strategy = "horizontal",
+      -- layout_strategy = "vertical",
       layout_config = { height = 0.95, width = 0.95 },
       path_display = { "truncate" },
+      -- preview_cutoff = 10,
+    },
+    pickers = {
+      lsp_definitions = {
+        show_line = false,
+      },
+      lsp_references = {
+        show_line = false,
+      },
     },
   },
 }
