43e56beae63da462b46738f6513c239331ebab25
Blade Boles <bladerboles@gmail.com>
2024-05-08 22:04:06 -0600

    fix: issue with preview width on telescope, stop the auto close on neotree as it was breaking opening the tree to the current file

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -52,17 +52,17 @@ require("lazy").setup({
     },
   },
 })
-require("neo-tree").setup({
-  event_handlers = {
-
-    {
-      event = "file_opened",
-      handler = function(file_path)
-        -- auto close
-        -- vimc.cmd("Neotree close")
-        -- OR
-        require("neo-tree.command").execute({ action = "close" })
-      end,
-    },
-  },
-})
+-- require("neo-tree").setup({
+--   event_handlers = {
+--
+--     {
+--       event = "file_opened",
+--       handler = function(file_path)
+--         -- auto close
+--         -- vimc.cmd("Neotree close")
+--         -- OR
+--         require("neo-tree.command").execute({ action = "close" })
+--       end,
+--     },
+--   },
+-- })
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,8 +4,8 @@ return {
   -- For major updates, this must be adjusted manually.
   opts = {
     defaults = {
-      layout_strategy = "horizontal",
-      layout_config = { height = 0.95, width = 0.95, preview_width = 0.4 },
+      -- layout_strategy = "horizontal",
+      layout_config = { height = 0.95, width = 0.95 },
       path_display = { "truncate" },
     },
   },
