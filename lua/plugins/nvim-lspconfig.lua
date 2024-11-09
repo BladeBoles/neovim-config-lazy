1b27173fce8cec4d7e33d7da0d35b243c9873f80
Blade Boles <bladerboles@gmail.com>
2024-11-08 21:55:22 -0700

    feat: update from arch

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -8,6 +8,7 @@ local util = require("lspconfig/util")
 return {
   "neovim/nvim-lspconfig",
   opts = {
+    inlay_hints = { enabled = false },
     servers = {
       tsserver = {
         init_options = {
